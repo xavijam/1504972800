bfb026e42c6507a47da5252b31d9dfeec5f1f6e5
xavijam <xavijam@gmail.com>
2016-10-09 22:44:14 +0200

    Hello translations

--- app/_assets/js/home.js
+++ app/_assets/js/home.js
@@ -1,23 +1,51 @@
-$(function(){ 
-
-	var baseLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', { attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>' });
-	var labelsLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', { attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>' });
-
-	var map = L.map($('.js-churchMap')[0], {
-		attribution: '',
-		scrollWheelZoom: false,
-		center: [40.527476, -3.919085],
-		zoom: 16
-	});
+$(function () {
 	
-	map.addLayer(baseLayer);
-	map.addLayer(labelsLayer);
-
-	var churchIcon = L.icon({
-    iconUrl: '../img/church.png',
+	createMap({
+		el: $('.js-churchMap')[0],
+		center: [40.527476, -3.919085],
+		iconUrl: '../img/church.png',
     iconSize: [50, 57],
     iconAnchor: [25, 28]
 	});
-
-	L.marker([40.527476, -3.919085], { icon: churchIcon }).addTo(map);
+	createMap({
+		el: $('.js-banquetMap')[0],
+		center: [40.6257519, -4.1069981],
+		iconUrl: '../img/losarcos.png',
+    iconSize: [70, 50],
+    iconAnchor: [35, 25]
+	});
 });
+
+function createMap (opts) {
+	var map = L.map(opts.el, {
+		scrollWheelZoom: false,
+		center: opts.center,
+		zoom: opts.zoom || 16
+	});
+	
+	addBasemapToMap(map);
+	createIcon({
+		center: opts.center,
+		map: map,
+		iconUrl: opts.iconUrl,
+    iconSize: opts.iconSize,
+    iconAnchor: opts.iconAnchor
+	});
+}
+
+function addBasemapToMap (map) {
+	var baseLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', { attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>' });
+	var labelsLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', { attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>' });
+	map.addLayer(baseLayer);
+	map.addLayer(labelsLayer);
+}
+
+function createIcon (opts) {
+	var icon = L.icon({
+    iconUrl: opts.iconUrl,
+    iconSize: opts.iconSize,
+    iconAnchor: opts.iconAnchor
+	});
+
+	L.marker(opts.center, { icon: icon }).addTo(opts.map);
+}
