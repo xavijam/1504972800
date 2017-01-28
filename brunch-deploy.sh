d29418d45b4bac9163d04b0aa7073d7aac5235d3
xavijam <xavijam@gmail.com>
2017-01-28 11:33:30 +0100

    Changes

--- brunch-deploy.sh
+++ brunch-deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 echo 'Building page'
 brunch build
+echo 'Commit public folder'
+git add --all
+git commit -m "Committing public folder"
+git push origin new-page
 echo 'Publishing to GH-pages'
-git push origin `git subtree split --prefix public new-page`:refs/heads/gh-pages --force
+git push origin `git subtree split --prefix public new-page`:gh-pages --force
