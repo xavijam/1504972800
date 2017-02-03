2f0bd1bf8161216b260d144a1a44e72c39e96eaa
xavijam <xavijam@gmail.com>
2017-02-03 19:35:08 +0100

    Committing public folder

--- brunch-deploy.sh
+++ brunch-deploy.sh
@@ -4,6 +4,6 @@ brunch build
 echo 'Commit public folder'
 git add --all
 git commit -m "Committing public folder"
-git push origin new-page
+git push origin master
 echo 'Publishing to GH-pages'
-git push origin `git subtree split --prefix public new-page`:gh-pages --force
+git push origin `git subtree split --prefix public master`:gh-pages --force
