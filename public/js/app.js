2c771458088efc4b42d5cb215bfcb2f799f86ff3
xavijam <xavijam@gmail.com>
2017-04-04 21:37:57 +0200

    Added more hotels

--- public/js/app.js
+++ public/js/app.js
@@ -373,6 +373,24 @@ module.exports = [{
   name: 'El Escorial',
   desc: 'Camping Resort el Escorial',
   link: 'http://www.campingelescorial.com/'
+}, {
+  category: 'Las Rozas',
+  data: {
+    price: '75',
+    stars: 4
+  },
+  name: 'Las Rozas',
+  desc: 'Las Rozas Gran Hotel',
+  link: 'https://www.attica21hotels.com/'
+}, {
+  category: 'Las Rozas',
+  data: {
+    price: '44',
+    stars: 3
+  },
+  name: 'Las Rozas',
+  desc: 'B&B Sidorme Hotel Las Rozas',
+  link: 'https://www.sidorme.com/hotel/madrid-las-rozas/'
 }];
 
 });
