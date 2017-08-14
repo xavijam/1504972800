12c35a27bca0b058243a02ee1d85c29db1977aaf
Javier Álvarez Medina <xavijam@gmail.com>
2017-08-14 15:58:56 +0200

    Remove links

--- app/js/travel-items.js
+++ app/js/travel-items.js
@@ -6,7 +6,7 @@ module.exports = [
     desc: 'honeymoon.items.renting.desc',
     cost: '~850',
     imageURL: 'http://farm6.static.flickr.com/5026/5629801428_1a7d254810_b.jpg',
-    itemURL: 'honeymoon.items.renting.url'
+    // itemURL: 'honeymoon.items.renting.url'
   },
   {
     type: 'honeymoon.leisure',
@@ -15,7 +15,7 @@ module.exports = [
     cost: '~125',
     imageURL: 'http://www.backpackerguide.nz/wp-content/uploads/2015/04/2cJVDjo.jpg',
     wikipediaURL: 'honeymoon.items.franz-josef.wikipedia',
-    itemURL: 'honeymoon.items.coromandel.url'
+    // itemURL: 'honeymoon.items.coromandel.url'
   },
   {
     type: 'honeymoon.leisure',
@@ -24,7 +24,7 @@ module.exports = [
     cost: '~300',
     wikipediaURL: 'honeymoon.items.franz-josef.wikipedia',
     imageURL: 'http://exp.cdn-hotels.com/hotels/1000000/90000/83700/83615/83615_72_z.jpg',
-    itemURL: 'honeymoon.items.franz-josef.url'
+    // itemURL: 'honeymoon.items.franz-josef.url'
   },
   {
     type: 'honeymoon.expense',
@@ -33,7 +33,7 @@ module.exports = [
     cost: '~180',
     wikipediaURL: 'honeymoon.items.ferry.wikipedia',
     imageURL: 'http://s-media-cache-ak0.pinimg.com/736x/c0/c2/cb/c0c2cb31e09703f4bc749d2f59f9fb8a.jpg',
-    itemURL: 'honeymoon.items.ferry.url'
+    // itemURL: 'honeymoon.items.ferry.url'
   },
   {
     type: 'honeymoon.leisure',
@@ -42,7 +42,7 @@ module.exports = [
     cost: '~120',
     wikipediaURL: 'honeymoon.items.hobbiton.wikipedia',
     imageURL: 'https://twistedsifter.files.wordpress.com/2015/03/hobbiton-movie-set-tour-new-zealand-2.jpg?w=800&h=534',
-    itemURL: 'honeymoon.items.hobbiton.url'
+    // itemURL: 'honeymoon.items.hobbiton.url'
   },
   {
     type: 'honeymoon.leisure',
@@ -51,7 +51,7 @@ module.exports = [
     cost: '~130',
     wikipediaURL: 'honeymoon.items.milford-sound.wikipedia',
     imageURL: 'http://photo980x880.mnstatic.com/a1caf2ac7c25b47264c262c3f66fd26a/milford-sound.jpg',
-    itemURL: 'honeymoon.items.milford-sound.url'
+    // itemURL: 'honeymoon.items.milford-sound.url'
   },
   {
     type: 'honeymoon.leisure',
@@ -60,7 +60,7 @@ module.exports = [
     cost: '~120',
     wikipediaURL: 'honeymoon.items.abel-tasman.wikipedia',
     imageURL: 'http://www.absolutenewzealand.com/wp-content/uploads/Abel-Tasman-National-Park2.jpg',
-    itemURL: 'honeymoon.items.abel-tasman.url'
+    // itemURL: 'honeymoon.items.abel-tasman.url'
   },
   {
     type: 'honeymoon.leisure',
@@ -69,7 +69,7 @@ module.exports = [
     cost: '~160',
     wikipediaURL: 'honeymoon.items.maori.wikipedia',
     imageURL: 'http://cache-graphicslib.viator.com/graphicslib/thumbs674x446/2295/SITours/rotorua-maori-hangi-dinner-and-performance-in-rotorua-359846.jpg',
-    itemURL: 'honeymoon.items.maori.url'
+    // itemURL: 'honeymoon.items.maori.url'
   },
   {
     type: 'honeymoon.expense',
@@ -78,7 +78,7 @@ module.exports = [
     cost: '~400',
     wikipediaURL: 'honeymoon.items.campervan-park.wikipedia',
     imageURL: 'http://blog.shareacamper.co.nz/wp-content/uploads/2015/09/Camping-in-New-Zealand-Where-to-Camp-Where-to-Stay-What-to-Expect-1050x700.jpg',
-    itemURL: 'honeymoon.items.campervan-park.url'
+    // itemURL: 'honeymoon.items.campervan-park.url'
   },
   {
     type: 'honeymoon.leisure',
@@ -87,7 +87,7 @@ module.exports = [
     cost: '~40',
     wikipediaURL: 'honeymoon.items.oamaru.wikipedia',
     imageURL: 'https://s-media-cache-ak0.pinimg.com/originals/7b/1b/89/7b1b893ccd0dcca603fd5736b8d8281c.jpg',
-    itemURL: 'honeymoon.items.oamaru.url'
+    // itemURL: 'honeymoon.items.oamaru.url'
   },
   {
     type: 'honeymoon.leisure',
@@ -96,7 +96,7 @@ module.exports = [
     cost: '~210',
     wikipediaURL: 'honeymoon.items.kaikoura.wikipedia',
     imageURL: 'http://www.moatrek.com/sites/default/files/inline-images/Whale-Watching-Kaikoura.jpg',
-    itemURL: 'honeymoon.items.kaikoura.url'
+    // itemURL: 'honeymoon.items.kaikoura.url'
   },
   {
     type: 'honeymoon.leisure',
@@ -105,7 +105,7 @@ module.exports = [
     cost: '~90',
     wikipediaURL: 'honeymoon.items.waitomo.wikipedia',
     imageURL: 'https://media-cdn.tripadvisor.com/media/photo-s/09/31/de/0c/waitomo-glowworm-caves.jpg',
-    itemURL: 'honeymoon.items.waitomo.url'
+    // itemURL: 'honeymoon.items.waitomo.url'
   },
   {
     type: 'honeymoon.leisure',
@@ -114,7 +114,7 @@ module.exports = [
     cost: '~125',
     wikipediaURL: 'honeymoon.items.auckland.wikipedia',
     imageURL: 'http://static.thousandwonders.net/Sky.Tower.original.33603.jpg',
-    itemURL: 'honeymoon.items.auckland.url'
+    // itemURL: 'honeymoon.items.auckland.url'
   },
   {
     type: 'honeymoon.expense',
